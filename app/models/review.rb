e503e3e2e9c65dd9dbd19cc9f2164d6d1e15a06a
Courtney Schild <courtneyschild@gmail.com>
2020-01-17 14:47:41 -0800

    use shoulda-matchers to write tests for validations; write validations to pass tests.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :product
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :content_body, presence: true
+  validates :rating, presence: true
+  validates_length_of :content_body, minimum: 50
+  validates_length_of :content_body, maximum: 250
 end
