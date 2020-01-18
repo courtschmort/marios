c6f5a778fc7037c1e0f6911bbd49d07675c1078e
Courtney Schild <courtneyschild@gmail.com>
2020-01-17 17:18:36 -0800

    add scopes.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  scope :most_reviews, -> {(
+    select("products.id, products.name, products.cost, products.country_of_origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    )}
+  scope :three_most_recent, -> { order(created_at: :desc).limit(3)}
+  scope :search, -> (country_of_origin_parameter) { where("country_of_origin like ?", "%#{country_of_origin_parameter}%")}
   validates :name, presence: true
   validates :cost, presence: true
   validates :country_of_origin, presence: true
