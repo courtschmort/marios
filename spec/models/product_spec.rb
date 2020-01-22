98cc5c12132228d8cc8158fb6859247a92ee30a5
Courtney Schild <courtneyschild@gmail.com>
2020-01-21 21:30:42 -0800

    revise test with cost and country_of_origin to pass failing test.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
   it("titleizes the name of a product") do
-    product = Product.create({name: "specialty food product"})
+    product = Product.create({name: "specialty food product", cost: 10, country_of_origin: "United States"})
     expect(product.name).to(eq("Specialty Food Product"))
   end
 end
